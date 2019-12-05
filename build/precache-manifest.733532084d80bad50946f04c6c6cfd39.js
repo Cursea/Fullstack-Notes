83c8b20c366dbae12b044dc4eddd787e887ef7ec
Marco <marcoburman@gmail.com>
2019-12-05 15:24:30 +0000

    Copy frontend prod build to backend

---
+++ build/precache-manifest.733532084d80bad50946f04c6c6cfd39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d75736834693cf7a177e8ec68fde76e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d799d84476cbe47eecb",
+    "url": "/static/css/main.5aae45cd.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "9d799d84476cbe47eecb",
+    "url": "/static/js/main.79b0d246.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
