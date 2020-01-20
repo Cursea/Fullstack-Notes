b046366b128cae4e063e30871059082d91dc11e4
Marco <marcoburman@gmail.com>
2020-01-20 23:57:48 +0000

    Begin project structure modification for Node.js best practices

---
+++ build/precache-manifest.d106024857fb284294f0cdfe27764e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379bb6a0566fda3d12a0e6bfb821483a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76ed636d47105bc6d095",
+    "url": "/static/css/main.5aae45cd.chunk.css"
+  },
+  {
+    "revision": "f862b8ae29a877f86b13",
+    "url": "/static/js/2.d57ac5dd.chunk.js"
+  },
+  {
+    "revision": "098d96cd5327dd3fc6707a77e047f86b",
+    "url": "/static/js/2.d57ac5dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76ed636d47105bc6d095",
+    "url": "/static/js/main.d36bc263.chunk.js"
+  },
+  {
+    "revision": "1762926e7182ad4a09aa",
+    "url": "/static/js/runtime-main.4f6e4bfe.js"
+  }
+]);
